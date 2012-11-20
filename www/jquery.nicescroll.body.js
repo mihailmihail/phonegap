44c9d936bd50c5234cd984ec3c0e489dd8cffbba
Alexander <alexandr.abashin@codex-soft.com>
2012-11-20 15:00:57 +0200

    povar

---
+++ www/jquery.nicescroll.body.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("body").niceScroll();
+	$("#home_page_refrigerator .ui-content").niceScroll({cursorcolor:"#0F0",boxzoom:true});
+	$("#SelectedIngredientsPage .ui-content").niceScroll({cursorcolor:"#0F0",boxzoom:true});
+	$("#RecipesListByIngredients .ui-content").niceScroll({cursorcolor:"#0F0",boxzoom:true});
+	$("#recipeFullInformation .ui-content").niceScroll({cursorcolor:"#0F0",boxzoom:true});
+	$("#SubIngredientPage .ui-content").niceScroll({cursorcolor:"#0F0",boxzoom:true});
+	$("#InformationApplication .ui-content").niceScroll({cursorcolor:"#0F0",boxzoom:true});
+});
